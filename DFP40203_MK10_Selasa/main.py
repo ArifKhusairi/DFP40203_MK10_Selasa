02c3728d5ac6c7d368c9390cd21c6bb96061f624
ArifKhusairi <soyayos93@gmail.com>
2023-11-07 11:17:59 +0800

    update1

--- DFP40203_MK10_Selasa/main.py
+++ DFP40203_MK10_Selasa/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import glob
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    senarai_fail = glob.glob("../DFP40203_MK10_Selasa/*.txt")
+    for nama_fail in senarai_fail:
+        print(nama_fail)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
