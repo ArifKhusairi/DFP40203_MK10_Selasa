5b4068d43361d3fd5e44ddaeee477b9b64cdb8cc
ArifKhusairi <soyayos93@gmail.com>
2023-11-07 11:51:41 +0800

    update2

--- DFP40203_MK10_Selasa/main.py
+++ DFP40203_MK10_Selasa/main.py
@@ -1,7 +1,22 @@
 import glob
+import os
+
+
+def rename_file(nama_lama, nama_baru):
+    lokasi_fail = f"../DFP40203_MK10_Selasa/{nama_lama}.txt"
+    destinasi_fail = f"../DFP40203_MK10_Selasa/{nama_baru}.txt"
+    os.rename(lokasi_fail, destinasi_fail)
 
 
 def main():
+    senarai_fail = glob.glob("../DFP40203_MK10_Selasa/*.txt")
+    for nama_fail in senarai_fail:
+        print(nama_fail)
+
+    nama_lama = input("masukkan nama lama : ")
+    nama_baru = input("masukkan nama baru : ")
+    rename_file(nama_lama, nama_baru)
+
     senarai_fail = glob.glob("../DFP40203_MK10_Selasa/*.txt")
     for nama_fail in senarai_fail:
         print(nama_fail)
